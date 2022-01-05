34296bc717138989b350e91ba28ecf9b9fc134c9
GabErves <Gabrielle.Erves@cix.csi.cuny.edu>
2022-01-04 23:14:02 -0500

    added more css and matched the home page to the NYC page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   }
